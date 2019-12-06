f255eca330d1f33f2d6cb92507e2d114efe2b624
Anthony Harris <anthony@ah.id.au>
2019-12-06 16:23:34 +0000

    Package as a JPMS module.

---
+++ src/main/java/com.rtfparserkit/module-info.java
@@ -0,0 +1,8 @@
+module com.rtfparserkit {
+    exports com.rtfparserkit.converter.text;
+    exports com.rtfparserkit.parser;
+    exports com.rtfparserkit.rtf;
+
+    requires java.base;
+    requires java.xml;
+}

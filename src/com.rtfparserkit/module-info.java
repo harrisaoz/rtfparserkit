9700dc021c02e13eb4788fbc3b992b4a813b619d
Anthony Harris <anthony@ah.id.au>
2019-12-10 14:02:49 +0000

    Convert to JPMS module (Java 9+).

--- src/com.rtfparserkit/module-info.java
+++ src/com.rtfparserkit/module-info.java
@@ -2,6 +2,7 @@ module com.rtfparserkit {
     exports com.rtfparserkit.converter.text;
     exports com.rtfparserkit.parser;
     exports com.rtfparserkit.rtf;
+    exports com.rtfparserkit.utils;
 
     requires java.base;
     requires java.xml;
